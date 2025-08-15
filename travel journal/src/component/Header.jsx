a19abe17ac557474d01eedd14e8b437002c201e8
vivek prajapati <vivek011prajapati@gmail.com>
2025-08-15 16:03:28 +0530

    import img

--- travel journal/src/component/Header.jsx
+++ travel journal/src/component/Header.jsx
@@ -1,8 +1,9 @@
 import React from "react";
+import earth from "../assets/earth-svgrepo-com.svg"
 const Header = () => {
     return (
         <header>
-            <img src="../assets/earth-svgrepo-com.svg"/>
+             <img src={earth} alt="Earth" />
             <h3>my travel journal</h3>
         </header>
     )
