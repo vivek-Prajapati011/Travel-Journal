25bc2dd7d8c0d11d562a34b2699ac9db852aa274
vivek prajapati <vivek011prajapati@gmail.com>
2025-08-15 15:54:17 +0530

    set img and heading

--- travel journal/src/component/Header.jsx
+++ travel journal/src/component/Header.jsx
@@ -1,5 +1,11 @@
 import React from "react";
 const Header = () => {
+    return (
+        <header>
+            <img src="../assets/earth-svgrepo-com.svg"/>
+            <h3>my travel journal</h3>
+        </header>
+    )
 
 }
 export default Header
