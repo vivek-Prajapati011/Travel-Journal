910f29b10e61942d0c52d280d1dcbfa373943ba8
vivek prajapati <vivek011prajapati@gmail.com>
2025-08-16 09:23:59 +0530

    card component logic

--- travel journal/src/component/Card.jsx
+++ travel journal/src/component/Card.jsx
@@ -1,5 +1,22 @@
 import React from "react";
-const Card = () => {
-
-}
-export default Card
+const Card = (props) => {
+  return (
+   <div className="card-container">
+  <div className="card-img">
+    <img src={props.imageUrl} alt={props.title} />
+  </div>  
+  <div className="card-content">
+    <div>
+      <div className="card-location">
+        <span>{props.countryCode}</span>
+        <a href={props.mapLink}>View on Google Maps</a>
+      </div>
+      <h2 className="card-title">{props.title}</h2>
+      <p className="card-dates">{props.startDate} - {props.endDate}</p>
+      <p className="card-description">{props.description}</p>
+    </div>
+  </div>
+</div>
+  );
+};
+export default Card;
