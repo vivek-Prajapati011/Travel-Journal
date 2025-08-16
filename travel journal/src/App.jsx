027b1106fadb774c10223b7c5db36f088ef3f2a1
vivek prajapati <vivek011prajapati@gmail.com>
2025-08-16 09:23:37 +0530

    render card component

--- travel journal/src/App.jsx
+++ travel journal/src/App.jsx
@@ -1,19 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Header from './component/Header'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Header from "./component/Header";
+import data from "./data";
+import Card from "./component/Card";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-     <div>
+      <div className="container">
         <Header />
-     </div>
+        <div>
+          {data.map((item) => (
+            <Card
+              key={item.id}
+              title={item.title}
+              location={item.location}
+              imageUrl={item.imageUrl}
+              description={item.description}
+            />
+          ))}
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
