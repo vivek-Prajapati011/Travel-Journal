f17de209ae30118c3977a60587e5a9b9fa576da4
vivek prajapati <vivek011prajapati@gmail.com>
2025-08-16 07:43:47 +0530

    add dummy data

---
+++ travel journal/src/data.js
@@ -0,0 +1,112 @@
+export default data [
+  {
+    id: 1,
+    title: "Mount Fuji",
+    location: "Japan",
+    googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
+    startDate: "12 Jan, 2023",
+    endDate: "24 Jan, 2023",
+    description:
+      "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). It is the most visited tourist site in Japan, famous for its symmetrical cone.",
+    imageUrl: "https://source.unsplash.com/WLxQvbMyfas"
+  },
+  {
+    id: 2,
+    title: "Sydney Opera House",
+    location: "Australia",
+    googleMapsUrl: "https://goo.gl/maps/5B2dZ1DkzRmK4y6C8",
+    startDate: "27 May, 2023",
+    endDate: "8 Jun, 2023",
+    description:
+      "The Sydney Opera House is a multi-venue performing arts center at Sydney Harbour. It's one of the most recognizable and photographed buildings in the world.",
+    imageUrl: "https://source.unsplash.com/JmuyB_LibRo"
+  },
+  {
+    id: 3,
+    title: "Geirangerfjord",
+    location: "Norway",
+    googleMapsUrl: "https://goo.gl/maps/kx4t5XErWQ9XJ4Vx7",
+    startDate: "01 Oct, 2024",
+    endDate: "18 Nov, 2024",
+    description:
+      "The Geiranger Fjord is a UNESCO World Heritage Site in the Sunnmøre region. Known for deep blue waters and majestic cliffs with cascading waterfalls.",
+    imageUrl: "https://source.unsplash.com/3PeSjpLVtLg"
+  },
+  {
+    id: 4,
+    title: "Grand Canyon",
+    location: "USA",
+    googleMapsUrl: "https://goo.gl/maps/x3b5uR1S7X5m39Zw8",
+    startDate: "10 Apr, 2023",
+    endDate: "17 Apr, 2023",
+    description:
+      "A natural wonder in Arizona, carved by the Colorado River, offering breathtaking vistas and hiking trails.",
+    imageUrl: "https://source.unsplash.com/eNoeWZkO7Zc"
+  },
+  {
+    id: 5,
+    title: "Santorini",
+    location: "Greece",
+    googleMapsUrl: "https://goo.gl/maps/tQ1J3jvTg9b9W6zE9",
+    startDate: "05 Jun, 2023",
+    endDate: "15 Jun, 2023",
+    description:
+      "A volcanic island in the Cyclades, known for white-washed houses, blue-domed churches, and sunsets over the caldera.",
+    imageUrl: "https://source.unsplash.com/9wg5jCEPBsw"
+  },
+  {
+    id: 6,
+    title: "Machu Picchu",
+    location: "Peru",
+    googleMapsUrl: "https://goo.gl/maps/dVjEYcR7Y5oM6t2V6",
+    startDate: "20 Aug, 2023",
+    endDate: "28 Aug, 2023",
+    description:
+      "An Incan citadel set high in the Andes Mountains, famous for its dry-stone walls and panoramic views.",
+    imageUrl: "https://source.unsplash.com/oMpAz-DN-9I"
+  },
+  {
+    id: 7,
+    title: "Bali",
+    location: "Indonesia",
+    googleMapsUrl: "https://goo.gl/maps/3j9F4r5P9uZ3ZjAW8",
+    startDate: "14 Feb, 2023",
+    endDate: "25 Feb, 2023",
+    description:
+      "An Indonesian paradise known for forested volcanic mountains, beaches, coral reefs, and vibrant culture.",
+    imageUrl: "https://source.unsplash.com/A-NVHPka9Rk"
+  },
+  {
+    id: 8,
+    title: "Petra",
+    location: "Jordan",
+    googleMapsUrl: "https://goo.gl/maps/y2GmRn6tWw22kUq86",
+    startDate: "11 Sep, 2023",
+    endDate: "15 Sep, 2023",
+    description:
+      "An ancient city carved into pink sandstone cliffs, also known as the Rose City, and one of the New Seven Wonders of the World.",
+    imageUrl: "https://source.unsplash.com/1K8pIbIrhkQ"
+  },
+  {
+    id: 9,
+    title: "Eiffel Tower",
+    location: "France",
+    googleMapsUrl: "https://goo.gl/maps/2E6E6gXYA5y7G4tX7",
+    startDate: "01 Mar, 2023",
+    endDate: "04 Mar, 2023",
+    description:
+      "The Eiffel Tower is an iconic iron structure in Paris, visited by millions annually for its views of the city.",
+    imageUrl: "https://source.unsplash.com/6VhPY27jdps"
+  },
+  {
+    id: 10,
+    title: "Banff National Park",
+    location: "Canada",
+    googleMapsUrl: "https://goo.gl/maps/YuNQJkfkLxEo4Ksv9",
+    startDate: "12 Jul, 2023",
+    endDate: "22 Jul, 2023",
+    description:
+      "Canada’s oldest national park, located in the Rocky Mountains, known for turquoise lakes and snowy peaks.",
+    imageUrl: "https://source.unsplash.com/VinlTUsVgUo"
+  }
+];
